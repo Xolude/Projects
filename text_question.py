acf1c0e2c1aaae8f6ccef89cf41972ef522bb505
Xolude <44958103+Xolude@users.noreply.github.com>
2019-12-17 11:03:15 -0700

    Update text_question.py

--- text_question.py
+++ text_question.py
@@ -5,7 +5,7 @@ import email, time
 
 user = "your_email"  #Make sure you have POP enabled for external apps with google, as well as having IMAP enabled
 pas = "your_password" #https://support.google.com/mail/answer/7104828?hl=en <- Use this to enable POP
-phonenumber = "4068905642" #Don't forget area code
+phonenumber = "your_phonenumber" #Don't forget area code
 sms_gateway = (str(phonenumber) + "@vtext.com") #Change suffix "@vtext" to your corresponding provider
 smtp = "smtp.gmail.com" 
 imap = "imap.gmail.com"
@@ -37,11 +37,11 @@ def search(key,value,con): #Search for messages from specific sender
 
 def fetch():  #Fetch and/or refresh any new incoming messages
     global con, result, data, raw
-    con = imaplib.IMAP4_SSL(imap)
-    con.login(user,pas)
-    con.select('INBOX')
-    result, data = con.fetch(b'3','(RFC822)')
-    raw = email.message_from_bytes(data[0][1])
+    con = imaplib.IMAP4_SSL(imap) #Establish imap connection
+    con.login(user,pas) #Log into email server
+    con.select('INBOX') #Grab default inbox
+    result, data = con.fetch(b'3','(RFC822)') #Grab message in byte form
+    raw = email.message_from_bytes(data[0][1]) #Turn message into text
 
 def get_emails(result_bytes): #Make a list of messages to be used by other functions
     msgs = []
@@ -57,7 +57,7 @@ def ask_question(question,ans1,ans2): #Sends question and awaits response, as we
     send_msg(str(question))
 
     while answer == False:
-        time.sleep(10)  #Wait a bit
+        time.sleep(5)  #Wait a bit
         fetch()  #Refresh messages
         messages = get_emails(search('FROM',sms_gateway,con))   #Check messages \/
         for i in messages:
